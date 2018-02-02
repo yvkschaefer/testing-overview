fff7b4a0d0560024e4f309c2903f13f58f087979
Kara Schaefer <yvkschaefer@gmail.com>
2018-02-02 18:19:28 -0500

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <a href="/"><img src="../assets/logo.png" alt="Mylo logo"></a>
+</template>
+
+<script>
+
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../main.scss';
+img {
+  height: 50px;
+  position: absolute;
+  left: 5%;
+  top: 3%;
+}
+</style>
