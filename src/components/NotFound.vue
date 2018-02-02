d9db8112b4aa5f270f05c8a68f5969e93e5340f1
Kara Schaefer <yvkschaefer@gmail.com>
2018-02-02 18:15:35 -0500

    linter errors rm hooked up Header component with nav SlideOne.vue working much better styles

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,11 +1,16 @@
 <template>
-  <h1>Page Not Found</h1>
+  <main>
+    <Header />
+    <h1>Page Not Found</h1>
+  </main>
 </template>
 
 <script>
 
+import Header from './Header'
 export default {
-  name: 'NotFound'
+  name: 'NotFound',
+  components: { Header }
 }
 </script>
 
--- src/components/SlideOne.vue
+++ src/components/SlideOne.vue
@@ -1,32 +1,64 @@
 <template>
-  <a href="/2">
-    <h1>Some Testing Types</h1>
-    <ul>
-      <li>Unit</li>
-      <li>Snapshot</li>
-      <li>Functional</li>
-      <li>Regression</li>
-      <li>Performance</li>
-      <li>Security</li>
-      <li>Load</li>
-      <li>Black box</li>
-      <li>Smoke</li>
-    </ul>
-  </a>
+  <main>
+    <Header />
+    <a href="/2">
+      <h1>Some Testing Types</h1>
+      <ul>
+        <li v-for="item in items" :key="item.id">{{item.title}}</li>
+      </ul>
+    </a>
+  </main>
 </template>
 
 <script>
 
+import Header from './Header'
 export default {
-  name: 'SlideOne'
+  name: 'SlideOne',
+  components: { Header },
+  data () {
+    return {
+      items: [
+        { title: 'Unit' },
+        { title: 'Snapshot' },
+        { title: 'Functional' },
+        { title: 'Regression' },
+        { title: 'Performance' },
+        { title: 'Security' },
+        { title: 'Load' },
+        { title: 'Black box' },
+        { title: 'Smoke' }
+      ]
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 @import '../main.scss';
-a {
-  height: 100vh;
-  width: 100vw;
-  text-decoration: none;
+h1 {
+  transform: scale(1.4);
+  padding-left: 2vw;
+}
+ul {
+  padding-top: 2vh;
+  list-style-type: none;
+  display: inline-block;
+  margin: auto;
+}
+li {
+  background-color: $mylo-blue;
+  color: #fff;
+  padding: 5px;
+  border-radius: 6px;
+  font-size: 4vh;
+  font-weight: bold;
+  width: 30vw;
+  margin-bottom: 5px;
+}
+li:hover {
+  background-color: #ffe64c;
+  color: #723fab;
+  transform: scale(1.2);
 }
 </style>
