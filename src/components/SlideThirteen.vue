c9661999e79113236cc345860c421f71595fc7f3
Matthew Young <myoung@mylo.ai>
2018-02-09 10:21:48 -0500

    add slide 13

--- src/components/SlideThirteen.vue
+++ src/components/SlideThirteen.vue
@@ -1,1 +1,27 @@
-SlideThirteen
+<template>
+  <main>
+    <Header />
+    <a href="/13">
+      <h1>Where do we go from here?</h1>
+      <ul>
+        <li>Helping me helping you!</li>
+        <li>Precommit hooks to run basic tests</li>
+        <li>Regression testinggggggggg!!!!</li>
+        <li>Automate all the things!</li>
+      </ul>
+    </a>
+  </main>
+</template>
+
+<script>
+
+import Header from './Header'
+export default {
+  name: 'SlideThirteen',
+  components: { Header }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../main.scss';
+</style>
